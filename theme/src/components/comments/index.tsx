0fcd43819887ece719f65d7f6293398b58043bfd
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2020-05-31 00:29:03 +0530

    Update index.tsx

--- theme/src/components/comments/index.tsx
+++ theme/src/components/comments/index.tsx
@@ -1,7 +1,6 @@
 import React, {FunctionComponent} from "react";
 
-<script defer src="https://cdn.commento.io/js/commento.js"></script>
-<div id="commento"></div>
+
 
 const Comments: FunctionComponent = () => <></>;
 
